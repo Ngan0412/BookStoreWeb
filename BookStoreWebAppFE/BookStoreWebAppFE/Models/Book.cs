44ae14004f6cb0f2c9a5a0efa48b57aac0ea391c
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2025-06-15 13:34:35 +0700

    Convert noti from English to Vietnamese

--- BookStoreWebAppFE/BookStoreWebAppFE/Models/Book.cs
+++ BookStoreWebAppFE/BookStoreWebAppFE/Models/Book.cs
@@ -6,34 +6,28 @@ namespace BookStoreWebAppFE.Models
     public  class Book
     {
         public Guid id { get; set; }
-        [Required(ErrorMessage = "Không Được Để Trống!")]
+
+        [Required(ErrorMessage = "Mã ISBN là bắt buộc.")]
         [StringLength(13, MinimumLength = 13, ErrorMessage = "Phải nhập 13 ký tự!")]
         public string Isbn { get; set; } = null!;
-        [Required(ErrorMessage =  "Không Được Để Trống !")]
+        [Required(ErrorMessage = "Tiêu đề là bắt buộc.")]
         [StringLength(200)]
         public string Title { get; set; }
-        [GuidNotEmpty(ErrorMessage = "Không Được Để Trống!")]
-
+        [GuidNotEmpty(ErrorMessage = "Thể loại là bắt buộc.")]
         public Guid categoryId { get; set; }
-        [GuidNotEmpty(ErrorMessage = "Không Được Để Trống!")]
-
+        [GuidNotEmpty(ErrorMessage = "Tác giả là bắt buộc.")]
         public Guid authorId { get; set; }
-        [GuidNotEmpty(ErrorMessage = "Không Được Để Trống!")]
-
+        [GuidNotEmpty(ErrorMessage = "Nhà xuất bản là bắt buộc.")]
         public Guid publisherId { get; set; }
-        [Required(ErrorMessage = "Không Được Để Trống !")]
-
+        [Required(ErrorMessage = "Năm xuất bản là bắt buộc.")]
         public int YearOfPublication { get; set; }
-        [Required(ErrorMessage = "Không Được Để Trống !")]
-
+        [Required(ErrorMessage = "Số lượng là bắt buộc.")]
         public int Quantity { get; set; }
-        [Required(ErrorMessage = "Không Được Để Trống !")]
-
+        [Required(ErrorMessage = "Hình ảnh là bắt buộc.")]
         public string Image { get; set; }
-        [Required(ErrorMessage = "Không Được Để Trống !")]
-
+        [Required(ErrorMessage = "Giá là bắt buộc.")]
         public decimal Price { get; set; }
-        [Required(ErrorMessage =  "Không Được Để Trống !")]
+        //[Required(ErrorMessage =  "")]
         public bool isDeleted { get; set; }
 
     }
